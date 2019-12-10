886acefa726aa73825c3f32a5a960adf288aa0e6
Patina <patina.her@gmail.com>
2019-12-10 23:12:14 +0800

    Add app 'bot'; add class Events in bot/views

---
+++ bot/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+import os
+import logging
+import slack
+import ssl as ssl_lib
+import certifi
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
+
+class Events(APIView):
+    def post(self, request, *args, **kwargs):
+        slack_message = request.data
+        if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response(status=status.HTTP_200_OK)
+        
+        # verification challenge
+        if slack_message.get('type') == 'url_verification':        #
+            return Response(data=slack_message,                    #
+                            status=status.HTTP_200_OK)             #
+
+        return Response(status=status.HTTP_200_OK)
