2a67a57d02487ee8162b938d39b9d02c2a56d3f9
Patina <patina.her@gmail.com>
2019-12-14 15:26:35 +0800

    print SLACK_VERIFICATION_TOKEN

--- bot/views.py
+++ bot/views.py
@@ -18,6 +18,8 @@ SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
 class Events(APIView):
     def post(self, request, *args, **kwargs):
         slack_message = request.data
+        print(slack_message)
+        print("SLACK_VERIFICATION_TOKEN =", SLACK_VERIFICATION_TOKEN)
         if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
