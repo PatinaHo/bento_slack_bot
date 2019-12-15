52ebb42bdf67380facb61a3d213172e062ba335c
Patina <patina.her@gmail.com>
2019-12-15 16:42:42 +0800

    Add: greetins logic

--- bot/views.py
+++ bot/views.py
@@ -14,6 +14,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 SLACK_VERIFICATION_TOKEN = os.environ['SLACK_VERIFICATION_TOKEN']
+SLACK_BOT_USER_TOKEN     = os.environ['SLACK_BOT_USER_TOKEN']
+# Client = SlackClient(SLACK_BOT_USER_TOKEN)
+Client = slack.WebClient(token=SLACK_BOT_USER_TOKEN)
 
 # SLACK_VERIFICATION_TOKEN = getattr(settings, 'SLACK_VERIFICATION_TOKEN', None)
 
@@ -27,5 +30,24 @@ class Events(APIView):
         if slack_message.get('type') == 'url_verification':
             return Response(data=slack_message.get('challenge'),
                             status=status.HTTP_200_OK)
+
+        # greet bot
+        if 'event' in slack_message:                              #4
+            event_message = slack_message.get('event')            #
+            
+            # ignore bot's own message
+            if event_message.get('subtype') == 'bot_message':     #5
+                return Response(status=status.HTTP_200_OK)        #
+            
+            # process user's message
+            user = event_message.get('user')                      #6
+            text = event_message.get('text')                      #
+            channel = event_message.get('channel')                #
+            bot_text = 'Hi <@{}> :wave:'.format(user)             #
+            if 'hi' in text.lower():                              #7
+                Client.api_call(method='chat.postMessage',        #8
+                                channel=channel,                  #
+                                text=bot_text)                    #
+                return Response(status=status.HTTP_200_OK)        #9
 
         return Response(status=status.HTTP_200_OK)
